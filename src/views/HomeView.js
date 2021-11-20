225a9228349a1d434ac8a77e6eabcc1e5ec56ae3
Mustafa ÖZTÜRK <bay.mustafa.37@hotmail.com>
2021-11-20 22:38:37 +0300

    Searc and home page edit

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { ScrollView } from "react-native";
-import { Layout, Head } from "../components/elements";
-import { Box, Text } from "../components/base";
+import {ScrollView} from "react-native";
+import {Layout, Head} from "../components/elements";
+import {Box, Text} from "../components/base";
 import {
-  HomeHeader,
-  GridPlaylist,
-  CarouselPlaylist,
+    HomeHeader,
+    GridPlaylist,
+    CarouselPlaylist,
 } from "../components/partials";
 
 const HomeView = () => {
-  return (
-    <Layout>
-      <HomeHeader />
+    return (
+        <Layout>
+            <HomeHeader/>
 
-      <GridPlaylist />
+            <GridPlaylist/>
 
-      <Box mb={25} />
-      <CarouselPlaylist title="Your shows" />
-      <Box mb={25} />
-      <CarouselPlaylist title="Recommended for today" />
-      <Box mb={25} />
-      <CarouselPlaylist title="Recently played" />
-      <Box mb={25} />
-      <CarouselPlaylist title="Uniquely yours" />
-    </Layout>
-  );
+            <Box mb={25}/>
+            <CarouselPlaylist title="Your shows"/>
+            <Box mb={25}/>
+            <CarouselPlaylist title="Recommended for today"/>
+            <Box mb={25}/>
+            <CarouselPlaylist title="Recently played"/>
+            <Box mb={25}/>
+            <CarouselPlaylist title="Uniquely yours"/>
+        </Layout>
+    );
 };
 
 export default HomeView;
--- src/views/SearchView.js
+++ src/views/SearchView.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { Box, Text, Button } from "../components/base";
+import { Box, Text, Button, Input } from "../components/base";
 import { colors } from "../theme";
-import { Layout } from "../components/elements";
+import { Layout, Head } from "../components/elements";
 
 const SearchView = () => {
   return (
     <Layout>
-      <Text color={colors.white}>SEARCH PAGE</Text>
-      <Button>
-        <Text>----</Text>
-      </Button>
+      
+      <Head title="Search"  mb={3} />  
+
+      <Input bg={colors.white} p={3} placeholder="Artists, song, or podcasts" />
+
+      <Head title="Your top genres" fontSize={16} my={3} />
+
+      
+
+
     </Layout>
   );
 };
